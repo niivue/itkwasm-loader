649d70490574bf61478a096d02a69ee040bf5195
Matt McCormick <matt@fideus.io>
2025-10-08 10:49:50 -0400

    test: load volume in dev server by default

--- src/setup-niivue.ts
+++ src/setup-niivue.ts
@@ -8,11 +8,11 @@ export async function setupNiivue(element: HTMLCanvasElement) {
   // useItkWasmLoaders(nv)
   useAllItkWasmLoaders(nv)
 
-  // await nv.loadImages([
-  //   {
-  //     url: '/LIDC2.mha'
-  //   }
-  // ])
+  await nv.loadImages([
+    {
+      url: '/LIDC2.mha'
+    }
+  ])
 
   // await nv.loadMeshes([
   //   {
